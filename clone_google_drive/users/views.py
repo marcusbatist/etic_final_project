279c6a092a020405136cc9ae66081121f3ece43f
Marcus Batista <marcus.batista.2003@gmail.com>
2024-06-12 22:02:38 +0100

    update

--- clone_google_drive/users/views.py
+++ clone_google_drive/users/views.py
@@ -1,3 +1,39 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+
+
+
+def register(request):
+    if request.method == "GET":
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+       
+        user = User.objects.filter(username=username).first()
+
+    if user:
+        return HttpResponse('Já existe um usuário com esse username!!')
+       
+    return HttpResponse({username,email,password})
+
+    #Anchor else 
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            return HttpResponse('autenticado')
+        else:
+            return HttpResponse('Email ou Password estão inválidos, por favor tente novamente! Obrigado!')
